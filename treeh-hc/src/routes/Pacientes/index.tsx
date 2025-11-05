5e8aa0a4ea73f423bebb222b42e6cc09148f522b
Henrique Martins <hrqmartins129@gmail.com>
2025-11-05 17:39:27 -0300

    feat: Cria a estrutura estática do componente Pacientes

--- treeh-hc/src/routes/Pacientes/index.tsx
+++ treeh-hc/src/routes/Pacientes/index.tsx
@@ -11,3 +11,13 @@ interface Paciente {
   peso: number;
   altura: number;
 }
+
+export default function Pacientes() {
+  return (
+    <main className="max-w-7xl mx-auto px-6 py-12 min-h-screen">
+      <h1 className="text-3xl font-bold text-gray-800 mb-8">
+        Gerenciamento de Pacientes
+      </h1>
+    </main>
+  );
+}
