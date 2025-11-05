96869cd1a28716d483cef121b198c75f441efba7
Henrique Martins <hrqmartins129@gmail.com>
2025-11-05 17:33:51 -0300

    feat: Exibe nome do usuário no título de boas-vindas

--- treeh-hc/src/routes/Dashboard/index.tsx
+++ treeh-hc/src/routes/Dashboard/index.tsx
@@ -1,12 +1,12 @@
 import { useAuth } from '../../contexts/AuthContext';
 
 export default function Dashboard() {
-  const { usuario } = useAuth(); // Adiciona hook
+  const { usuario } = useAuth();
 
   return (
     <main className="max-w-7xl mx-auto px-6 py-12 min-h-screen">
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
-        Bem-vindo(a)!
+        Bem-vindo(a), {usuario?.nome}! {/* Atualiza o h1 */}
       </h1>
     </main>
   );
