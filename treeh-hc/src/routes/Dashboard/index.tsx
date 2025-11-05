fa26f8ea9b3d16c7371a3d11440dad8efb80b2b2
Henrique Martins <hrqmartins129@gmail.com>
2025-11-05 17:34:19 -0300

    feat: Exibe o tipo de usuário na dashboard

--- treeh-hc/src/routes/Dashboard/index.tsx
+++ treeh-hc/src/routes/Dashboard/index.tsx
@@ -6,8 +6,12 @@ export default function Dashboard() {
   return (
     <main className="max-w-7xl mx-auto px-6 py-12 min-h-screen">
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
-        Bem-vindo(a), {usuario?.nome}! {/* Atualiza o h1 */}
+        Bem-vindo(a), {usuario?.nome}!
       </h1>
+      {/* Adiciona o parágrafo com o tipo de usuário */}
+      <p className="text-xl text-gray-600 mb-8">
+        Você está logado como: <strong>{usuario?.tipoUsuario}</strong>
+      </p>
     </main>
   );
 }
