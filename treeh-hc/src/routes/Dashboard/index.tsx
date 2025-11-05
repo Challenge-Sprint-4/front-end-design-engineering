0a1b1324e33ea9b15b30e2d762e7e00721c2a641
Henrique Martins <hrqmartins129@gmail.com>
2025-11-05 17:33:03 -0300

    feat: Estrutura jsx estática da página de dashboard

---
+++ treeh-hc/src/routes/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <main className="max-w-7xl mx-auto px-6 py-12 min-h-screen">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Bem-vindo(a)!
+      </h1>
+    </main>
+  );
+}
