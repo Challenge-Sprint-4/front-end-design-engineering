76cfbe3dd1211c6f94f8c694c249ff595d6ee75f
Henrique Martins <hrqmartins129@gmail.com>
2025-11-05 17:33:26 -0300

    feat: Integra dashboard com hook useAuth

--- treeh-hc/src/routes/Dashboard/index.tsx
+++ treeh-hc/src/routes/Dashboard/index.tsx
@@ -1,4 +1,8 @@
+import { useAuth } from '../../contexts/AuthContext';
+
 export default function Dashboard() {
+  const { usuario } = useAuth(); // Adiciona hook
+
   return (
     <main className="max-w-7xl mx-auto px-6 py-12 min-h-screen">
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
